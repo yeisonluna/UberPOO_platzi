c9e3f8952f5e2171ac97785ffe4f2192d9a3200e
Yeison Luna <yeisonluna99@hotmail.com>
2023-03-21 20:44:49 -0500

    Actualizacion de clases en Java uso de encapsulamiento para añadir pasajeros en UberX

--- Java/Car.java
+++ Java/Car.java
@@ -2,14 +2,32 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;  
+    private Integer passenger;  
 
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
+        System.out.println("Passenger: " + passenger);
     }
     
     void printDataCar(){
-        System.out.println("License: " + license + " Driver: " + driver.name + " Passengers: " + passenger);
+        if(passenger != null){
+            System.out.println("License: " + license + " Driver: " + driver.name + " Passengers: " + passenger);
+        }
+        
+    }
+
+    public Integer getPassenger(){
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger){
+        if(passenger == 4){
+            this.passenger = passenger;    
+        }else{
+            System.out.println("Necesitas ingresar 4 pasajeros");
+
+        }
+        
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args){
         System.out.println("Hola Mundo");
         UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Spark");
-        uberX.passenger = 4;
+        uberX.setPassenger(2);
         uberX.printDataCar();
 
         /*Car car2 = new Car("QWE567", new Account("Andrea Sanchez", "ANDA231"));
